6c693928937158c9f4605abaa1b1f7c8243538b7
5P4RK3R <mahendransparker@gmail.com>
2023-03-29 11:43:51 +0530

    freelancer scrapy

--- Scraper/Scrapy/crawler/spiders/a99acres.py
+++ Scraper/Scrapy/crawler/spiders/a99acres.py
@@ -5,11 +5,11 @@ from scrapy.selector import Selector
 
 class A99acresSpider(scrapy.Spider):
     name = '99acres'
-    allowed_domains = ['https://www.99acres.com/independent-house-for-rent-in-india-ffid']
-    start_urls = ['http://https://www.99acres.com/independent-house-for-rent-in-india-ffid/']
+    allowed_domains = ['https://www.99acres.com/']
+    start_urls = ['http://https://www.99acres.com/independent-house-for-rent-in-india-ffid']
 
     def parse(self, response):
-        lands = Selector(response).xpath('//div[@data-label="SEARCH"]')
+        lands = Selector(response).xpath("//div[@data-label='SEARCH']")
         # lands = response.css('div.pageComponent')
         print("lands",lands)
         for land in lands:
--- Scraper/Scrapy/crawler/spiders/freelancer.py
+++ Scraper/Scrapy/crawler/spiders/freelancer.py
@@ -1,5 +1,6 @@
 import scrapy
 import time
+from traceback import format_exc
 # from ..item import CrawlerItem
 # from scrapy.loader import itemLoader
 
@@ -9,30 +10,39 @@ class FreelancerSpider(scrapy.Spider):
     start_urls = ['https://www.freelancer.com/jobs']
 
     def parse(self, response):
-        jobs = response.css('div.JobSearchCard-item')
-        for job in jobs:
-            # il = itemLoader(item = CrawlerItem(),selector = job)
+        try:
+            jobs = response.css('div.JobSearchCard-item')
+            for job in jobs:
+                # il = itemLoader(item = CrawlerItem(),selector = job)
 
-            # il.add_css('title','a.JobSearchCard-primary-heading-link::text')
-            # il.add_css('desc','p.JobSearchCard-primary-description::text')
-            # il.add_css('price','div.JobSearchCard-secondary-price::text')
+                # il.add_css('title','a.JobSearchCard-primary-heading-link::text')
+                # il.add_css('desc','p.JobSearchCard-primary-description::text')
+                # il.add_css('price','div.JobSearchCard-secondary-price::text')
 
-            # yield il.load_item()
+                # yield il.load_item()
+                price = job.css('div.JobSearchCard-secondary-price::text')
+                title = job.css('a.JobSearchCard-primary-heading-link::text')
+                desc = job.css('p.JobSearchCard-primary-description::text')
+                yield {
+                    'title': title.get().strip() if title else "",
+                    'desc': desc.get().strip() if desc else "",
+                    'price': price.get().strip() if price else ""
+                }
 
-            yield {
-                'title': job.css('a.JobSearchCard-primary-heading-link::text').get().strip(),
-                'desc': job.css('p.JobSearchCard-primary-description::text').get().strip(),
-                'price':job.css('div.JobSearchCard-secondary-price::text').get().strip()
-            }
-
-        total_pages = int(response.css('#bottom-pagination a:last-child::attr(href)').get().strip('jobs/'))
-        next_page = response.css('#bottom-pagination a:nth-last-child(2)::attr(href)').get()
-        page = int(response.css('ul.Breadcrumbs-list li:last-child span::text').get())
-        # print(total_pages,next_page,page, page is not total_pages)
-        print("total_pages",total_pages)        
-        if page is not total_pages:
-            print("---------------Next Page------------")
-            next_page = response.urljoin(next_page)
-            print(next_page)
-            yield scrapy.Request(next_page, callback = self.parse)
+            total_pages = int(response.css('#bottom-pagination a:last-child::attr(href)').get().strip('jobs/'))
+            next_page = response.css('#bottom-pagination a:nth-last-child(2)::attr(href)').get()
+            # next_page = int(response.css('#bottom-pagination a:nth-last-child(2)::attr(href)').get().strip('jobs/'))
+            page = int(response.css('ul.Breadcrumbs-list li:last-child span::text').get())
+            # print("31test",next_page)
+            print("31test",total_pages,next_page,page, page is not total_pages)
+            print("total_pages",total_pages)        
+            if page is not total_pages:
+                print("---------------Next Page------------")
+                print("next_page",next_page)
+                next_page = response.urljoin(f"https://www.freelancer.com{next_page}")
+                print("next_page",next_page)
+                # yield response.follow(next_page,self.parse)
+                yield scrapy.Request(next_page, callback = self.parse)
+        except:
+            print(format_exc())
 
