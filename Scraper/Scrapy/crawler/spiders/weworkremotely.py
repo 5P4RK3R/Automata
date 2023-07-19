0ebdc314f0a71c411e6b7e65750a120e7307997e
5P4RK3R <mahendransparker@gmail.com>
2023-07-19 20:24:07 +0530

    weworkremotely

--- Scraper/Scrapy/crawler/spiders/weworkremotely.py
+++ Scraper/Scrapy/crawler/spiders/weworkremotely.py
@@ -10,4 +10,21 @@ class WeworkremotelySpider(scrapy.Spider):
         self.links = response.css('section.jobs li.view-all a::attr(href)').extract()
         self.follows = response.css('section.jobs ul li a::attr(href)').extract()
         self.logger.info(f"links {self.links}")
-        self.logger.info(f"links {self.follows}")
+        self.logger.info(f"follows {self.follows}")
+        self.logger.info(f"length {len(self.follows)}")
+        for link in self.links:
+            url = response.urljoin(f"https://weworkremotely.com{link}")
+            self.logger.info(f"next_page {url}")
+            yield response.follow(url,callback = self.parse_jobs)
+
+    def parse_jobs(self, response):
+        self.follows.extend(response.css('section.jobs ul li a::attr(href)').extract())
+        self.logger.info(f"follows {self.follows}")
+        self.logger.info(f"len {len(self.follows)}")
+        for follow in self.follows:
+            url = response.urljoin(f"https://weworkremotely.com{follow}")
+            self.logger.info(f"next_page {url}")
+            yield response.follow(url,callback = self.parse_job)
+
+    def parse_job(self, response):
+        pass
