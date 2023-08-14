f399a77ed4e2bb0ce1f3ef9fdfb23b0542419706
5P4RK3R <mahendransparker@gmail.com>
2023-08-14 13:46:11 +0530

    freelancer spider

--- Scraper/Scrapy/crawler/spiders/freelancer.py
+++ Scraper/Scrapy/crawler/spiders/freelancer.py
@@ -55,8 +55,8 @@ class FreelancerSpider(scrapy.Spider):
             # total_pages = int(response.css('#bottom-pagination a:last-child::attr(href)').get().strip('jobs/'))
             next_page = response.css('#bottom-pagination a:nth-last-child(2)::attr(href)').get()
             last_page = response.css('#bottom-pagination a:nth-last-child(1)::attr(href)').get()
-            self.logger.info(next_page)
-            self.logger.info(last_page)
+            # self.logger.info(next_page)
+            # self.logger.info(last_page)
             # next_page = int(response.css('#bottom-pagination a:nth-last-child(2)::attr(href)').get().strip('jobs/'))
             # page = int(response.css('ul.Breadcrumbs-list li:last-child span::text').get())
             # print("31test",next_page)
@@ -71,7 +71,7 @@ class FreelancerSpider(scrapy.Spider):
             #     yield response.follow(next_page,callback = self.parse) 
             # if next_page == last_page:
             for link in self.links:
-                self.logger.info(f"next_page {link}")
+                # self.logger.info(f"next_page {link}")
                 next_page = response.urljoin(f"https://www.freelancer.com{link}")
                 self.logger.info(f"next_page {link}")
                 yield response.follow(next_page,callback = self.parse_job) 
@@ -87,17 +87,22 @@ class FreelancerSpider(scrapy.Spider):
             self.logger.error(format_exc())
 
     def parse_job(self, response):
-        # il = ItemLoader(item = FreelancerItem(),selector = response)
-        # il.add_css('title','fl-heading.Heading span::text')
+        il = ItemLoader(item = FreelancerItem(),selector = response)
+        il.add_css('title','h1.PageProjectViewLogout-projectInfo-title::text')
+        il.add_css('description','div.PageProjectViewLogout-detail::text')
+        il.add_css('price','div.ProjectViewDetails-budget::text')
+        il.add_css('location','span[itemprop="addressLocality"]::text')
+        il.add_css('skills','div.Content::text')
+
         # # il.add_css('skills','fl-bit.ProjectViewDetailsSkills::text')
         # # il.add_css('raw_description','fl-bit.Card')
-        # yield il.load_item()
+        yield il.load_item()
         # skills = response.xpath('//fl-bit[@class="ProjectViewDetailsSkills"]')
-        title = response.xpath('//fl-text[@class="ng-star-inserted"]')
-        yield {
-            # "skills": skills
-            "title": title
-        }
+        # title = response.xpath('//fl-text[@class="ng-star-inserted"]')
+        # yield {
+        #     # "skills": skills
+        #     "title": title
+        # }
 
 # class FreelanceSpider(CrawlSpider):
 #     name = 'freelance'
